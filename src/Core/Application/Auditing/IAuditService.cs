230c01e388a149e40de3846db1f40a43aab715dd
baranacikgoz <baran-acikgoz@outlook.com>
2023-07-20 19:20:15 +0300

    Breaking: Disabled advanced auditing. CraetedOn CreatedBy etc. is still remain.

--- src/Core/Application/Auditing/IAuditService.cs
+++ src/Core/Application/Auditing/IAuditService.cs
@@ -1,6 +1,6 @@
-namespace FSH.WebApi.Application.Auditing;
+//namespace FSH.WebApi.Application.Auditing;
 
-public interface IAuditService : ITransientService
-{
-    Task<List<AuditDto>> GetUserTrailsAsync(Guid userId);
-}
+//public interface IAuditService : ITransientService
+//{
+//    Task<List<AuditDto>> GetUserTrailsAsync(Guid userId);
+//}
--- src/Infrastructure/Persistence/Configuration/Auditing.cs
+++ src/Infrastructure/Persistence/Configuration/Auditing.cs
@@ -1,15 +1,15 @@
-﻿using Finbuckle.MultiTenant.EntityFrameworkCore;
-using FSH.WebApi.Infrastructure.Auditing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿//using Finbuckle.MultiTenant.EntityFrameworkCore;
+//using FSH.WebApi.Infrastructure.Auditing;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace FSH.WebApi.Infrastructure.Persistence.Configuration;
+//namespace FSH.WebApi.Infrastructure.Persistence.Configuration;
 
-public class AuditTrailConfig : IEntityTypeConfiguration<Trail>
-{
-    public void Configure(EntityTypeBuilder<Trail> builder) =>
-        builder
-            .ToTable("AuditTrails", SchemaNames.Auditing);
+//public class AuditTrailConfig : IEntityTypeConfiguration<Trail>
+//{
+//    public void Configure(EntityTypeBuilder<Trail> builder) =>
+//        builder
+//            .ToTable("AuditTrails", SchemaNames.Auditing);
 
-    //.IsMultiTenant();
-}
+//    //.IsMultiTenant();
+//}
