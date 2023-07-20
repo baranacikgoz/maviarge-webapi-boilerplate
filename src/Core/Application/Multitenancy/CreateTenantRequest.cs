40a2472afb8b314a15506604b0ebce5df71999a7
baranacikgoz <baran-acikgoz@outlook.com>
2023-07-20 19:09:43 +0300

    Enhancement: Added PushNotificationsSettings in CreateTenantRequest as well

--- src/Core/Application/Multitenancy/CreateTenantRequest.cs
+++ src/Core/Application/Multitenancy/CreateTenantRequest.cs
@@ -1,3 +1,5 @@
+using FSH.WebApi.Application.Common.PushNotifications;
+
 namespace FSH.WebApi.Application.Multitenancy;
 
 public class CreateTenantRequest : IRequest<string>
@@ -7,6 +9,7 @@ public class CreateTenantRequest : IRequest<string>
     public string? ConnectionString { get; set; }
     public string AdminEmail { get; set; } = default!;
     public string? Issuer { get; set; }
+    public PushNotificationsSettings? PushNotificationsSettings { get; set; }
 }
 
 public class CreateTenantRequestHandler : IRequestHandler<CreateTenantRequest, string>
