6f30278c0964d2844f99cd9ff2ce00774e2674d0
baranacikgoz <baran-acikgoz@outlook.com>
2023-07-20 19:36:02 +0300

    Breaking: Made ISoftDelete optional

--- src/Migrators/Migrators.PostgreSQL/Migrations/Application/20230720163539_made_ISoftDelete_optional.Designer.cs
+++ src/Migrators/Migrators.PostgreSQL/Migrations/Application/20230720163539_made_ISoftDelete_optional.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FSH.WebApi.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Migrators.PostgreSQL.Migrations.Application
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230720163539_made_ISoftDelete_optional")]
+    partial class made_ISoftDelete_optional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Migrators/Migrators.PostgreSQL/Migrations/Application/20230720163539_made_ISoftDelete_optional.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.PostgreSQL.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class made_ISoftDelete_optional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                schema: "Catalog",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                schema: "Catalog",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                schema: "Catalog",
+                table: "Brands");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                schema: "Catalog",
+                table: "Brands");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeletedBy",
+                schema: "Catalog",
+                table: "Products",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                schema: "Catalog",
+                table: "Products",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeletedBy",
+                schema: "Catalog",
+                table: "Brands",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                schema: "Catalog",
+                table: "Brands",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
