701341f9d0a10b867c06213756b6d9043bd6e6c2
baranacikgoz <baran-acikgoz@outlook.com>
2023-07-27 11:47:21 +0300

    changed solution name

---
+++ src/Migrators/Migrators.PostgreSQL/Migrations/Tenant/20230725131240_fix_typo_in_field_name.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.PostgreSQL.Migrations.Tenant
+{
+    /// <inheritdoc />
+    public partial class fix_typo_in_field_name : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SmsSettings_ProviderType",
+                schema: "MultiTenancy",
+                table: "Tenants",
+                newName: "SmsSettings_Provider");
+
+            migrationBuilder.RenameColumn(
+                name: "SmsSettings_Password",
+                schema: "MultiTenancy",
+                table: "Tenants",
+                newName: "SmsSettings_PasswordOrAuthKey");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SmsSettings_Provider",
+                schema: "MultiTenancy",
+                table: "Tenants",
+                newName: "SmsSettings_ProviderType");
+
+            migrationBuilder.RenameColumn(
+                name: "SmsSettings_PasswordOrAuthKey",
+                schema: "MultiTenancy",
+                table: "Tenants",
+                newName: "SmsSettings_Password");
+        }
+    }
+}
